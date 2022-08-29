fdba789e67b80ee9c1dfcca4fbd3ff95648b130d
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2022-08-28 20:31:56 -0400

    playing around

---
+++ code/js/main.js
@@ -0,0 +1,26 @@
+
+// defines songs as 
+let var songs = [];
+
+function start(){ 
+
+    var link = readLine("What is the link of the playlist you'd like to import? ");
+    getSpotify(link);
+
+}
+
+//this function gets the songs from the spotify link and adds them to a list/array
+
+function getSpotify(link){
+
+    //gets the legth of the playlist, idk if it should go in the loop or before/after the loop
+    var COUNT = songs.length;
+
+    //count is the number of songs in the playlist
+    for(var i = 0; i < COUNT; i++){
+        
+        arr.push(title + "by" + artist);
+
+    }
+    
+}
